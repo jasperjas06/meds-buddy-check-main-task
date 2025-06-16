86100ab5541d934f7a38c0eef85d14cdcde91f1c
Jasper P <jas31059@gmail.com>
2025-06-16 11:06:47 +0530

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -11,12 +11,14 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    target: "es2018", 
   },
 }));
